b321c4fce5055bb613b12998c8b29b5ead59e187
Danillo <40369696+DanilloMedeiros@users.noreply.github.com>
2021-02-04 16:30:59 -0300

    Adição da busca por nome

--- model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -26,5 +26,9 @@ public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRep
 		return CpfUtil.validaCPF(cpf);
 	}
 	
-
+	public Cliente buscarClientePorNome(String nome) {
+		return clienteRepository.findByNome(nome);
+	}
+	
+	
 }
