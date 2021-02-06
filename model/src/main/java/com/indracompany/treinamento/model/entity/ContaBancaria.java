81d1bd2630b60b73fc979ef26eee3201d21469ff
CaioWallace <caiowgvo@gmail.com>
2021-02-06 11:57:26 -0300

    operacoes bancarias basicas

---
+++ model/src/main/java/com/indracompany/treinamento/model/entity/ContaBancaria.java
@@ -0,0 +1,39 @@
+package com.indracompany.treinamento.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "contas")
+@EqualsAndHashCode(callSuper = true)
+public class ContaBancaria extends GenericEntity<Long> {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_cliente_id", nullable = false)
+	private Cliente cliente;
+	
+	@Column(name = "agencia", nullable = false)
+	private String agencia;
+	
+	@Column(name = "numero", nullable = false)
+	private String numero;
+	
+	@Column(name = "saldo", nullable = false)
+	private double saldo;
+
+	
+}
