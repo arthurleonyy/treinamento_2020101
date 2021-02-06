41d682320c53463812a92bf940eb15bbd6f815cd
elielton silva <elieltonpb1@gmail.com>
2021-02-06 15:51:50 -0300

    elieltonSV

---
+++ model/src/main/java/com/indracompany/treinamento/model/dto/DepositoDTO.java
@@ -0,0 +1,15 @@
+package com.indracompany.treinamento.model.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class DepositoDTO extends GenericDTO {
+
+	private static final long serialVersionUID = 4320607655918916745L;
+	
+	private String agencia;
+	private String numeroConta;
+	private double valor;
+}
---
+++ model/src/main/java/com/indracompany/treinamento/model/dto/GenericDTO.java
@@ -0,0 +1,7 @@
+package com.indracompany.treinamento.model.dto;
+
+import java.io.Serializable;
+
+public class GenericDTO implements Serializable{
+
+}
