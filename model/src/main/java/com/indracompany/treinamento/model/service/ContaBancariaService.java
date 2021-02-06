861737784e1acbed74c9f9deaeb4aa2b5f8f0c1c
efmendes <efmendes@indracompany.com>
2021-02-06 11:50:17 -0300

    transferencia bancaria

--- model/src/main/java/com/indracompany/treinamento/model/service/ContaBancariaService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ContaBancariaService.java
@@ -1,16 +1,22 @@
 package com.indracompany.treinamento.model.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.indracompany.treinamento.exception.AplicacaoException;
 import com.indracompany.treinamento.exception.ExceptionValidacoes;
+import com.indracompany.treinamento.model.dto.TransferenciaBancarioDTO;
 import com.indracompany.treinamento.model.entity.ContaBancaria;
 import com.indracompany.treinamento.model.repository.ContaBancariaRepository;
 
 @Service
 public class ContaBancariaService extends GenericCrudService<ContaBancaria, Long, ContaBancariaRepository>{
 	
-	public void transferir()
+	@Transactional(rollbackFor = Exception.class)
+	public void transferir(TransferenciaBancarioDTO dto) {
+		this.sacar(dto.getAgenciaOrigem(), dto.getNumeroContaOrigem(), dto.getValor());
+		this.depositar(dto.getAgenciaDestino(), dto.getNumeroContaDestino(), dto.getValor());
+	}
 	
 	public void depositar (String agencia, String numeroConta, double valor) {
 		ContaBancaria conta = this.consultaConta(agencia, numeroConta);
