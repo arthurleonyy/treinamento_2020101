fa4c31b7226bcc9cd795251e832a8d53cf6dd182
unknown <sml.gomess@gmail.com>
2021-02-09 18:07:10 -0300

    Added conta bancaria

---
+++ model/src/main/java/com/indracompany/treinamento/model/dto/TransferenciaBancariaDTO.java
@@ -0,0 +1,21 @@
+package com.indracompany.treinamento.model.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class TransferenciaBancariaDTO extends GenericDTO {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4146773220700171235L;
+	
+	
+	private String agenciaOrigem;
+	private String numeroContaOrigem;
+	private String agenciaDestino;
+	private String numeroContaDestino;
+	private double valor;
+}
---
+++ model/src/main/java/com/indracompany/treinamento/model/entity/ContaBancaria.java
@@ -0,0 +1,38 @@
+package com.indracompany.treinamento.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "contas")
+@EqualsAndHashCode(callSuper = true)
+public class ContaBancaria extends GenericEntity<Long> {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_client_id", nullable = false)
+	private Cliente cliente;
+	
+	@Column(name = "agencia", nullable = false, length = 4)
+	private String agencia;
+	
+	@Column(name = "numero", nullable = false,  length = 6)
+	private String numero;
+	
+	@Column(name = "saldo", nullable = false)
+	private double saldo;
+	
+}
