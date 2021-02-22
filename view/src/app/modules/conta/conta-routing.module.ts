e7e03e0ca1e47507853d4fa55cf11744d18f4fe1
Pedro <pedrorhamon16@gmail.com>
2021-02-22 20:44:11 -0300

    Operacoes Incluso

---
+++ view/src/app/modules/conta/conta-routing.module.ts
@@ -0,0 +1,23 @@
+import { Component, NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContaComponent } from './pages/conta.component';
+import { OperacoesComponent } from './pages/operacoes.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContaComponent,
+    children: [
+      {
+      path:'operacoes',
+      component: OperacoesComponent,
+    }
+  ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ContaRoutingModule { }
---
+++ view/src/app/modules/conta/conta.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContaRoutingModule } from './conta-routing.module';
+import { ContaComponent } from './pages/conta.component';
+import { OperacoesComponent } from './pages/operacoes.component';
+
+@NgModule({
+  declarations: [
+    ContaComponent,
+    OperacoesComponent,
+  ],
+  imports: [
+    CommonModule,
+    ContaRoutingModule,
+  ]
+})
+export class ContaModule { }
