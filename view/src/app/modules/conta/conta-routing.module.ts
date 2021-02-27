6da3e519c62ada0fc2f41b0f17be503b1a21cf98
SammyJr21 <sammyjuniorgt@gmail.com>
2021-02-26 23:09:14 -0300

    Angular-26/02

---
+++ view/src/app/modules/conta/conta-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContaComponent } from './pages/conta.component';
+import { OperacoesComponent } from './pages/operacoes/operacoes.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContaComponent,
+    children: [
+      {
+        path: '',
+        component: OperacoesComponent,
+
+      },
+      {
+      path: 'operacoes',
+      component: OperacoesComponent,
+      }
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ContaRoutingModule { }
