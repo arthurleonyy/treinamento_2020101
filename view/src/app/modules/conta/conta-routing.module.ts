b59f60d903c996c0fd3da9370ef7602a7654e067
Arthur Leony <aldantas@indracompany.com>
2021-02-27 13:38:07 -0300

    Aula 2 parte 1

--- view/src/app/modules/conta/conta-routing.module.ts
+++ view/src/app/modules/conta/conta-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContaComponent } from './pages/conta.component';
+import { DepositarSacarComponent } from './pages/depositar-sacar/depositar-sacar.component';
+import { OperacoesComponent } from './pages/operacoes/operacoes.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ContaComponent,
+    children: [
+      {
+        path: '',
+        component: OperacoesComponent
+      },
+      {
+        path: 'operacoes',
+        component: OperacoesComponent
+      },
+      {
+        path: 'depositar',
+        component: DepositarSacarComponent
+      },
+    ]
   },
 ];
 
