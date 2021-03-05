73992ca48480dcfcaab19c7810eb02df9916dbc0
SammyJr21 <sammyjuniorgt@gmail.com>
2021-03-05 12:35:02 -0300

    atualizando

--- view/src/app/modules/conta/conta.module.ts
+++ view/src/app/modules/conta/conta.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ContaRoutingModule } from './conta-routing.module';
 import { ContaComponent } from './pages/conta.component';
 import { OperacoesComponent } from './pages/operacoes/operacoes.component';
+import { TransferenciaComponent } from './pages/transferencia/transferencia.component';
 
 @NgModule({
   declarations: [
     ContaComponent,
     OperacoesComponent,
+    TransferenciaComponent,
   ],
   imports: [
     CommonModule,
